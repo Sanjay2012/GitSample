35d0f741a2eb5a17259b5a54103253d42d210471
Sanjay Waware <sanjaywaware04@gmail.com>
2022-07-20 13:32:31 +0530

    first commit

---
+++ src/programs/SumOfNumbers.java
@@ -0,0 +1,15 @@
+package programs;
+
+public class SumOfNumbers {
+
+	public static void main(String[] args) {
+		System.out.println(SumOfNumbers.sum(10));
+		
+	}
+
+	public static double sum( int a){
+		double result=0;
+	return result= a*(a+1)/2;
+	}
+
+}
