961892b18b84452c0e26da8409c233aa325bd2e5
Sanjay Waware <sanjaywaware04@gmail.com>
2022-07-20 13:44:40 +0530

    swapping number program added for developer

---
+++ src/programs/SwapNumbers.java
@@ -0,0 +1,22 @@
+package programs;
+
+public class SwapNumbers {
+
+	public static void main(String[] args) {
+
+	}
+	
+	public static void sample(int a, int b) {
+		System.out.println("The value of a before swap is :"+a);
+		System.out.println("The value of a before swap is :"+a);
+		
+		// your codes goes here
+		
+		
+		System.out.println("The value of a after swap is :"+a);
+		System.out.println("The value of a after swap is :"+a);
+		
+		
+	}
+
+}
