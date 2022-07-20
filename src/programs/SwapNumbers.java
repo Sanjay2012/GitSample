14cb9a451950d3ef48e30b2635b234e6cab1cc01
Sanjay Waware <sanjaywaware04@gmail.com>
2022-07-20 13:56:56 +0530

    swap number logic updated

--- src/programs/SwapNumbers.java
+++ src/programs/SwapNumbers.java
@@ -3,18 +3,19 @@ package programs;
 public class SwapNumbers {
 
 	public static void main(String[] args) {
+		
+		SwapNumbers.sample(10, 20);
 
 	}
 	
 	public static void sample(int a, int b) {
 		System.out.println("The value of a before swap is :"+a);
-		System.out.println("The value of a before swap is :"+a);
+		System.out.println("The value of b before swap is :"+b);
 		
-		// your codes goes here
-		
+		b=a+b-(a=b);
 		
 		System.out.println("The value of a after swap is :"+a);
-		System.out.println("The value of a after swap is :"+a);
+		System.out.println("The value of b after swap is :"+b);
 		
 		
 	}
